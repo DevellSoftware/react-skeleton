87c0f59ff8ab949955209a1e8ae9f98f5b5aa2f4
Filip Gorny <filip.gorny@gmail.com>
2023-04-23 08:43:36 +0200

    Added browse layout

--- src/skeleton/layout/ContentBrowseLayout/ContentBrowseLayout.tsx
+++ src/skeleton/layout/ContentBrowseLayout/ContentBrowseLayout.tsx
@@ -3,13 +3,15 @@ import styles from "./ContentBrowseLayout.module.scss";
 
 export interface ContentBrowseLayoutProps {
   children: React.ReactNode;
+  sidebar?: React.ReactNode;
+  search?: React.ReactNode;
 }
 
 export const ContentBrowseLayout = (props: ContentBrowseLayoutProps) => {
   return (
     <main className={styles.ContentBrowseLayout}>
-      <aside className={styles.sidebar}>aside</aside>
-      <div className={styles.search}>search bar</div>
+      <aside className={styles.sidebar}>{props?.sidebar}</aside>
+      <div className={styles.search}>{props?.search}</div>
       <article className={styles.screen}>{props.children}</article>
     </main>
   );
